ac2175d5424825c7215149a0b7e9aefde55abb1c
kalinicham <kalinicham@ukr.net>
2019-02-27 23:12:01 +0200

    Action+

--- app/code/local/Kalinich/Action/Block/Action.php
+++ app/code/local/Kalinich/Action/Block/Action.php
@@ -3,6 +3,7 @@
 class Kalinich_Action_Block_Action extends Mage_Catalog_Block_Product_Abstract {
 
         public function getDefaultActionCollection(){
-
+            //TODO
+           return $this;
         }
 }
