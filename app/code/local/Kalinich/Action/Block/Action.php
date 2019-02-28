7afc25af0ff371d160f40be45541a0fdb61b82e1
kalin <kalinicham@ukr.net>
2019-02-28 09:51:06 +0200

    Tets commit in office and push github

--- app/code/local/Kalinich/Action/Block/Action.php
+++ app/code/local/Kalinich/Action/Block/Action.php
@@ -5,5 +5,6 @@ class Kalinich_Action_Block_Action extends Mage_Catalog_Block_Product_Abstract {
         public function getDefaultActionCollection(){
             //TODO
             return $this;
+// test commit in office and pull gidhub.com
         }
 }
