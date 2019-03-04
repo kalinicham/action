8f2de4f972e58377c2265ca6ec51d1955253c207
kalin <kalinicham@ukr.net>
2019-03-04 16:22:27 +0200

    Cron працює

--- app/code/local/Kalinich/Action/Model/Cron.php
+++ app/code/local/Kalinich/Action/Model/Cron.php
@@ -5,11 +5,35 @@ class Kalinich_Action_Model_Cron
 
     public function actionUpDateStatus()
     {
-        $actionsCollection = Mage::getModel('kalinich_action/post_action')->getCollection();
-        foreach ($actionsCollection as $action) {
-            var_dump($action);
-        }
-        return $this;
-    }
+        $date = strtotime(Mage::getModel('core/date')->gmtDate());
 
+        $actionsCollection = Mage::getModel('kalinich_action/post')->getCollection();
+
+        foreach ($actionsCollection as $action) {
+            $status = '0'; $end = null;
+            $start = strtotime($action->getStartDatetime());
+            if (!is_empty_date($action->getEndDatetime())) {
+                $end = strtotime($action->getEndDatetime()); }
+            switch (!is_null($end)) {
+                case true :
+                    if ($start < $date && $date < $end) {
+                        $status = '1';
+                    }elseif ($end < $date) {
+                        $status = '2';
+                    }elseif ($date < $start) {
+                        $status = '0';
+                    }
+                    break;
+                case false :
+                    if ($date < $start) {
+                        $status = '0';
+                    }else {
+                        $status = '1';
+                    }
+                    break;
+            }
+            $action->setStatus($status);
+            $action->save();
+        }
+    }
 }
